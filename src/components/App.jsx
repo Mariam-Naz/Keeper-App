78e09c104d37d8b6bf346f7f8e07369a23ccf9a3
unknown <mariamnaz212@gmail.com>
2020-06-28 02:53:59 +0500

    include map in keeper app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App(){
     return <div>
     <header><Header /></header>
-   <body><Note /></body>
+   <body>{notes.map(note=>
+       <Note key = {note.id} title ={note.title} content = {note.content}/>
+   )}</body>
     <footer><Footer /></footer>
     </div>
 }
---
+++ src/notes.js
@@ -0,0 +1,25 @@
+const notes = [{
+    id: 1,
+    title:"HTML",
+    content:"Hypertext Markup Language, a standardized system for tagging text files to achieve font, colour, graphic, and hyperlink effects on World Wide Web pages."
+},
+
+{
+    id:2,
+    title:"CSS",
+    content:"CSS stands for Cascading Style Sheets. CSS describes how HTML elements are to be displayed on screen, paper, or in other media. CSS saves a lot of work."
+},
+
+{
+    id:3,
+    title:"JAVASCRIPT",
+    content:"A JavaScript function is a block of code designed to perform a particular task. A JavaScript function is executed when 'something' invokes it (calls it)."
+},
+{
+    id:4,
+    title:"REACT",
+    content:"React is a JavaScript library created by Facebook. React is a User Interface (UI) library. React is a tool for building UI components."
+}
+]
+
+export default notes;
